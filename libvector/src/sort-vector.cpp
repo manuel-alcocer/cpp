bb3b98d084dfa4d3aba4449f7eeeb3b502eaabd0
Manuel Alcocer Jimenez <manuel@alcocer.net>
2017-07-20 22:00:20 +0200

    getopt added

--- libvector/src/sort-vector.cpp
+++ libvector/src/sort-vector.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
 #include <vector>
 #include <string>                   //stoi
+#include <stdlib.h>
+#include <unistd.h>
 #include "../headers/vectorutils.h"
 
-// pide el tamaño del vector
-void askVectorSize(int& v){
-    std::cout << "\nList size?: ";
-    std::cin >> v;
-}
+bool PRINTVECTS = 0;
 
 void makeQuicksort(const std::vector<int>& sortedVec, const std::vector<int>& unsortedVec){
     std::vector<int> vectorcopy = unsortedVec;
@@ -18,14 +16,16 @@ void makeQuicksort(const std::vector<int>& sortedVec, const std::vector<int>& un
     std::vector<unsigned> stats{0,0};
 
     // Ordenar mediante quicksort
-    std::cout << "\nQuicksort\n\n";
-    printVector(vectorcopy, "Unsorted list:");
+    std::cout << "\n* Quicksort\n";
+    if (PRINTVECTS)
+        printVector(vectorcopy, "Unsorted list:");
 
     quicksort(vectorcopy, 0, vectorcopy.size()-1, stats);
-    printVector(vectorcopy, "Sorted list  :");
-    std::cout << "\nQuicksort Stats:\n\n" <<
-        "Comps: " << stats[0] << '\n' <<
-        "Swaps: " << stats[1] << '\n';
+    if (PRINTVECTS)
+        printVector(vectorcopy, "Sorted list  :");
+    std::cout << "\tQuicksort Stats:\n" <<
+        "\t\tComps: " << stats[0] << '\n' <<
+        "\t\tSwaps: " << stats[1] << '\n';
 }
 
 void makeBubblesort(const std::vector<int>& sortedVec,
@@ -39,29 +39,33 @@ void makeBubblesort(const std::vector<int>& sortedVec,
     std::vector<unsigned> stats{0,0};
 
     // Ordenar mediante burbuja con flag de salida (default = true)
-    std::cout << "\n\nBubblesort\n\n";
+    std::cout << "\n* Bubblesort\n";
     vectorcopy = unsortedVec;
-    printVector(vectorcopy, "Unsorted list:");
+    if (PRINTVECTS)
+        printVector(vectorcopy, "\tUnsorted list:");
     bubbleSort(vectorcopy, stats, swpflag);
-    printVector(vectorcopy, "Sorted list  :");
-    std::cout << "\nBubblesort Stats:\n\n" <<
-        "Comps: " << stats[0] << '\n' <<
-        "Swaps: " << stats[1] << '\n';
+    if (PRINTVECTS)
+        printVector(vectorcopy, "\tSorted list  :");
+    std::cout << "\tBubblesort Stats:\n" <<
+        "\t\tComps: " << stats[0] << '\n' <<
+        "\t\tSwaps: " << stats[1] << '\n';
 }
 
 // función principal
-int main(int argc, char *argv[]){
+int main(int argc, char **argv){
     
     std::vector<int> sortedVec, unsortedVec;
-    
-    int vecsize = 0;
-    
-    if ( argc < 2){
-        askVectorSize(vecsize);
-    } else {
-        vecsize = std::stoi(argv[1]);
-    }
 
+    int opt;
+    int vecsize = 0;
+    while ((opt = getopt(argc, argv, "n:p")) != EOF)
+        switch (opt){
+            case 'n':
+                vecsize = std::stoi(optarg); break;
+            case 'p':
+                PRINTVECTS = true;
+        }
+    
     sortedVec = genSortedVec(vecsize);
     unsortedVec = genUnsortedVec(sortedVec);
 
